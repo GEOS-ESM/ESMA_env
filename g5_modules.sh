10edc0698df5241fbdaf87a5551a6aabaca9ab2c
Yonggang Yu <yonggang.g.yu@gmail.com>
2026-04-08 14:28:02 -0600

    add site=gs6150

--- g5_modules.sh
+++ g5_modules.sh
@@ -23,9 +23,15 @@ fi
 # Basedir
 export BASEDIR=$(csh $g5modules basedir)
 
+echo "g5modules = $g5modules"
+echo "MODULESHOME = $MODULESHOME"
+echo "BASEDIR= $BASEDIR"
+
+
 # UDUNITS2_XML_PATH
 arch=$(uname -s)
 export UDUNITS2_XML_PATH=$BASEDIR/$arch/share/udunits/udunits2.xml
+
 
 # Modules
 source $MODULESHOME/init/bash
